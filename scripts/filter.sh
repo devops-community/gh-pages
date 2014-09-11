4a9405e9a429d2591ff9462e92155cc4554d5f0d
Vincent Viallet <vincent.viallet@gmail.com>
2014-09-11 19:26:08 +0800

    Fixing typo

--- scripts/filter.sh
+++ scripts/filter.sh
@@ -4,17 +4,17 @@
 ####################
 
 if [ -z "$EXPECTED" ]; then
-  echo "Missing EXPECTED variable.' >&2
+  echo "Missing EXPECTED variable." >&2
   exit 1
 fi
 
 if [ -z "$VALUE" ]; then
-  echo "Missing VALUE variable.' >&2
+  echo "Missing VALUE variable." >&2
   exit 1
 fi
 
 if [ "$EXPECTED" != "$VALUE" ]; then
-  echo "The task has been filtered' >&2
+  echo "The task has been filtered" >&2
   echo "  - Expected: $EXPECTED" >&2
   echo "  - Received: $VALUE" >&2
   exit 1
